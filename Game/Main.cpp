856aea8faa47c36054057b5dd4ded0fef21fef1c
Ju0011 <yhs07240@naver.com>
2022-12-12 17:08:41 +0900

    12.12 mvc 모델 수정

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -5,154 +5,21 @@
 #include "View_Start.h"
 #include <algorithm>    //배열 복사
 #include "Sound.h"
+#include "Controller.h"
 
 using namespace std;
 
 int main()
 {
-
-    int pushCount = 0;
-
+    _setcursortype(_NOCURSOR);
+    clrscr();
     system("mode con: cols=98 lines=30");   // 화면 크기 조정
-    View* view = 0;
-    View_Start* start = 0;
     Sound* sound = 0;
-    
+    Controller* controller = 0;
+
     sound->Play();
     //sound->mp3Play();
-    start->logo();
-    
-    start->selectStage();
+    controller->controller();
 
-    _setcursortype(_NOCURSOR);
-
-    clrscr();
-
-    bool exit = false;
-    
-    int map_number = start->selectStage();
-    view->define_Map(map_number);
-
-    view->Inform();
-    
-
-    int start_x = 2, start_y = 4;
-    int oldx, oldy;
-    view->drawCharacter(start_x, start_y);
-
-    while (exit != true) {
-        view->drawCount(pushCount);
-
-        bool move = true;
-        int dx = 0, dy = 0;
-
-        int key = view->getkey();
-		int new_x = oldx = start_x;
-		int new_y = oldy = start_y;
-
-        switch (key) {
-        case M_LEFTKEY: dx = -1; break;
-        case M_UPKEY: dy = -1; break;
-        case M_DOWNKEY:  dy = 1; break;
-        case M_RIGHTKEY:  dx = 1; break;
-
-        case SPACE:
-            view->delCharacter(oldx, oldy);
-            view->drawMap(copy_mapData);
-            start_x = 2; start_y = 4;
-            view->drawCharacter(2, 4);
-            pushCount = 0;
-            break;
-
-        case N:
-        case n:
-            if (map_number > 0 && map_number < 5) {
-                start_x = 2; start_y = 4;
-
-                map_number++; view->define_Map(map_number);
-            }
-           
-            break;
-        case P:
-        case p:
-            if (map_number > 0 && map_number <= 6) {
-                start_x = 2; start_y = 4;
-
-                map_number--; view->define_Map(map_number);
-            }
-            break;
-        case ESC:
-            start->exit();
-            exit = true;
-        }
-
-        new_x = start_x + dx;
-        new_y = start_y + dy;
-
-
-        
-        switch (mapData[new_y][new_x]) {
-        case 0: //길
-            
-            break;
-        case 1: //벽
-        case 4: //골인지점
-            move = false;
-            break;
-
-        case 2: //공
-            if (mapData[new_y + dy][new_x + dx] == 0) { //길
-                mapData[new_y][new_x] = 0;  //길
-                mapData[new_y + dy][new_x + dx] = 2;    //공
-                view->drawCell(new_x + dx, new_y + dy, mapData);
-                pushCount++;
-            }
-            else if(mapData[new_y + dy][new_x + dx] == 4) { //골대
-                mapData[new_y][new_x] = 0;  //길
-                mapData[new_y + dy][new_x + dx] = 3;    //채워진 공
-                view->drawCell(new_x + dx, new_y + dy, mapData);
-                pushCount++;
-            }
-            else if (mapData[new_y + dy][new_x + dx] == 1) {    //벽
-                move = false;
-            }
-            break;
-
-        case 3: // 완성된 골
-            if (mapData[new_y + dy][new_x + dx] == 0) { //길
-                mapData[new_y][new_x] = 4;  //골대
-                mapData[new_y + dy][new_x + dx] = 2;    // 공
-                view->drawCell(new_x + dx, new_y + dy, mapData);
-                pushCount++;
-            }
-            else if (mapData[new_y + dy][new_x + dx] == 4) {    // 골대
-                mapData[new_y][new_x] = 4;  //골대
-                mapData[new_y + dy][new_x + dx] = 3;    // 채워진 공
-                view->drawCell(new_x + dx, new_y + dy, mapData);
-                pushCount++;
-            }
-            else if (mapData[new_y + dy][new_x + dx] == 1) {    //벽
-                move = false;
-            }
-            break;
-        case 5:
-            pushCount = 0;
-        }
-        
-        if (move) {
-            start_x = new_x;
-            start_y = new_y;
-            view->drawCell(new_x, new_y, mapData);
-            view->delCharacter(oldx, oldy);
-            view->drawCharacter(new_x, new_y);
-        }
-
-        if (view->success()) {
-            start_x = 2;
-            start_y = 4;
-            view->show_success(map_number);
-        }
-        
-        
-    }
+    return 0;
 }
